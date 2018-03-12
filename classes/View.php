19e4271aca76ebb29d504d7c018dfaa1cb2cfd2b
somushkin <scrollamount@gmail.com>
2018-03-12 16:16:55 +0200

    Output buffer

--- classes/View.php
+++ classes/View.php
@@ -5,14 +5,29 @@ class View
 	protected $data = [];
 	const PATH = __DIR__ . '/../views/';
 
-	public function display($template)
+	public function render($template)
 	{
 		foreach ($this->data as $key => $value) {
 			$$key = $value;
 		}
+		ob_start();
 		include View::PATH . $template;
-//		var_dump($this);
+		$content = ob_get_contents();
+		ob_end_clean();
+		return $content;
+		/*
+			функция подготавливает данные к выводу.
+			перед выводом документа данные буферизируются, 
+			где их можно предварительно обработать
+		*/
 	}
+
+	public function display($template)
+	{
+		echo $this->render($template);
+		// выводит обработанные данные в документ
+	}
+
 	public function __set($name, $value)
 	{
 		$this->data[$name] = $value;
